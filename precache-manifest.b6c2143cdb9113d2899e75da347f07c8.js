239747e165725a051304d303c26bc53aceb13dc1
The Quill Mac <thequillmac@Thes-MacBook-Pro.local>
2020-04-23 17:36:45 -0400

    Updates

---
+++ precache-manifest.b6c2143cdb9113d2899e75da347f07c8.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0c0735accab8dc186ff38c4a1f87a1a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3fdbf212e73761ef45e0",
+    "url": "/portfolio/static/css/2.ed315883.chunk.css"
+  },
+  {
+    "revision": "4d1549c34365e87ccfc2",
+    "url": "/portfolio/static/css/main.98dfedfd.chunk.css"
+  },
+  {
+    "revision": "3fdbf212e73761ef45e0",
+    "url": "/portfolio/static/js/2.84dad0da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.84dad0da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d1549c34365e87ccfc2",
+    "url": "/portfolio/static/js/main.29f7dbf0.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  },
+  {
+    "revision": "527c81de89a1938ae006e3101056ea68",
+    "url": "/portfolio/static/media/blackjack.527c81de.png"
+  },
+  {
+    "revision": "0673fce072a3134c098d9d253c0976d0",
+    "url": "/portfolio/static/media/codepen-icon.0673fce0.svg"
+  },
+  {
+    "revision": "889449e31190719bac49e10699663581",
+    "url": "/portfolio/static/media/css-icon.889449e3.svg"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/portfolio/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/portfolio/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/portfolio/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/portfolio/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/portfolio/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/portfolio/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/portfolio/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/portfolio/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/portfolio/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/portfolio/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/portfolio/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/portfolio/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/portfolio/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/portfolio/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/portfolio/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "50d509e87e37564b3a90971082ff696c",
+    "url": "/portfolio/static/media/github-icon.50d509e8.svg"
+  },
+  {
+    "revision": "22675d875b974395162d0bf70d204b23",
+    "url": "/portfolio/static/media/jeopardy-training.22675d87.png"
+  },
+  {
+    "revision": "d849bc2557228a905cb46a21f1819d90",
+    "url": "/portfolio/static/media/linkedin-icon.d849bc25.svg"
+  },
+  {
+    "revision": "cfee114b0b3de28827b98bee2ac0d6f0",
+    "url": "/portfolio/static/media/mongodb-icon.cfee114b.svg"
+  },
+  {
+    "revision": "705b987e58fa317cf913c181018040be",
+    "url": "/portfolio/static/media/nodejs-icon.705b987e.svg"
+  },
+  {
+    "revision": "4a49841e39ef0c829648a3127ef70214",
+    "url": "/portfolio/static/media/portfolio.4a49841e.png"
+  },
+  {
+    "revision": "4c5a5345e7d1cd117af2f1747d1d42e0",
+    "url": "/portfolio/static/media/react-icon.4c5a5345.svg"
+  },
+  {
+    "revision": "2117ce67c50028fba31dc06fffa32cb8",
+    "url": "/portfolio/static/media/recipe-rundown.2117ce67.png"
+  },
+  {
+    "revision": "9c48de4806c1ecd68be5e636a945a1d5",
+    "url": "/portfolio/static/media/spotify-playlists.9c48de48.png"
+  }
+]);
